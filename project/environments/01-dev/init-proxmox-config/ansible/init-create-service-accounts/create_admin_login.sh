d836518df9990859acfc1d1a2b12f08e92500590
Christian Talavera <ctalaveraw@gmail.com>
2022-11-01 01:10:36 -0400

    edited service account scripts to use variables

--- project/environments/01-dev/init-proxmox-config/ansible/init-create-service-accounts/create_admin_login.sh
+++ project/environments/01-dev/init-proxmox-config/ansible/init-create-service-accounts/create_admin_login.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+NEW_USERNAME="admin"
+
 # Make user account
 echo "Make user account..."
-useradd --shell /bin/bash --create-home --home-dir /home/admin admin
+useradd --shell /bin/bash --create-home --home-dir /home/$NEW_USERNAME $NEW_USERNAME
 
 # Give sudo access
 echo "Give sudo access"
-usermod -aG sudo admin
+usermod -aG sudo $NEW_USERNAME
 
 # Set password
 echo "Set password..."
-passwd admin
+passwd $NEW_USERNAME
