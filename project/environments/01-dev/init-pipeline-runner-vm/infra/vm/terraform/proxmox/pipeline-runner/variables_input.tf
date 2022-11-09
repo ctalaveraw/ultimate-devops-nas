27a5a2e7a59f14ce95d4531654d235e717e765bf
Christian Talavera <ctalaveraw@gmail.com>
2022-11-09 00:07:49 -0500

    cleaned up formatting

--- project/environments/01-dev/init-pipeline-runner-vm/infra/vm/terraform/proxmox/pipeline-runner/variables_input.tf
+++ project/environments/01-dev/init-pipeline-runner-vm/infra/vm/terraform/proxmox/pipeline-runner/variables_input.tf
@@ -1,64 +1,64 @@
 ## HTTP server configuration
 variable "http_host_ip" {
-    type = string
-    sensitive = false
+  type      = string
+  sensitive = false
 }
 
 variable "http_gateway_ip" {
-    type = string
-    sensitive = false
-    default = "192.168.1.1"
+  type      = string
+  sensitive = false
+  default   = "192.168.1.1"
 }
 
 # Proxmox connection configuration
 variable "proxmox_vm_name" {
-    type = string
-    sensitive = false
-    default = "pipeline-runner"
+  type      = string
+  sensitive = false
+  default   = "pipeline-runner"
 }
 
 variable "proxmox_vm_template_name" {
-    type = string
-    sensitive = false
-    default = "ubuntu-server-jammy"
+  type      = string
+  sensitive = false
+  default   = "ubuntu-server-jammy"
 }
 
 variable "proxmox_vm_disk_storage_pool" {
-    type = string
-    sensitive = false
-    default = "local-zfs"
+  type      = string
+  sensitive = false
+  default   = "local-zfs"
 }
 
 variable "proxmox_target_node" {
-    type = string
-    sensitive = false
-    default = "pve"
+  type      = string
+  sensitive = false
+  default   = "pve"
 }
 
 ## cloud-init configuration
 variable "cloudinit_ssh_username" {
-    type = string
-    sensitive = false
-    default = "root"
+  type      = string
+  sensitive = false
+  default   = "root"
 }
 
 variable "cloudinit_ssh_key" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 # Proxmox Secrets
 variable "proxmox_api_url" {
-    type = string
-    sensitive = false
+  type      = string
+  sensitive = false
 }
 
 variable "proxmox_api_token_id" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "proxmox_api_token_secret" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
