61bba36f3729a18a7f261ef2602d735d3057ef80
Christian Talavera <ctalaveraw@gmail.com>
2022-11-11 17:28:42 -0500

    edited name of SSH keygen script

--- project/environments/01-dev/init-pipeline-runner-vm/infra/image/packer/proxmox/create_ssh_key_pipeline_runner.sh
+++ project/environments/01-dev/init-pipeline-runner-vm/infra/image/packer/proxmox/create_ssh_key_pipeline_runner.sh
@@ -2,9 +2,9 @@
 
 echo -e '\n Making SSH Key... \n'
 sleep 1s
-ssh-keygen -f ~/.ssh/terraform_connect -t ed25519 -P ""
+ssh-keygen -f ~/.ssh/pipeline_runner -t ed25519 -P ""
 
-echo -e '\n Key saved to "~/.ssh/terraform_connect"... \n'
+echo -e '\n Key saved to "~/.ssh/pipeline_runner"... \n'
 sleep 1s
 
 echo -e 'Hashing algorithm is "Ed25519"... \n'
@@ -14,5 +14,5 @@ echo -e 'Created keypair is passwordless... \n'
 sleep 1s
 
 echo -e 'Showing the created public key below... \n'
-cat ~/.ssh/terraform_connect.pub
+cat ~/.ssh/pipeline_runner.pub
 sleep 1s
