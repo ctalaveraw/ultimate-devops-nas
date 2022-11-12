949c1a155003d1ec9f09d53a55bd1c0bb09b0abb
Christian Talavera <ctalaveraw@gmail.com>
2022-11-12 14:10:03 -0500

    replicated project backbone to NAS VM deployment

---
+++ project/environments/01-dev/init-ultimate-nas-vm/infra/vm/terraform/proxmox/nas-vm/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.2.9"
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.3"
+    }
+  }
+}
+
+provider "proxmox" {
+
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+
+  # (Optional) Skip TLS Verification
+  pm_tls_insecure = true
+
+}
