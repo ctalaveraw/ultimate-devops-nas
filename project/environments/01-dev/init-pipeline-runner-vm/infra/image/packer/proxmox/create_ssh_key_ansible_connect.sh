5f4172a8f1dd6243e70468e6a3e6f6805eba22a1
Christian Talavera <ctalaveraw@gmail.com>
2022-11-12 00:51:09 -0500

    added ansible service account to template

---
+++ project/environments/01-dev/init-pipeline-runner-vm/infra/image/packer/proxmox/create_ssh_key_ansible_connect.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e '\n Making SSH Key... \n'
+sleep 1s
+ssh-keygen -f ~/.ssh/ansible_connect -t rsa -P ""
+
+echo -e '\n Key saved to "~/.ssh/ansible_connect"... \n'
+sleep 1s
+
+echo -e 'Hashing algorithm is "Ed25519"... \n'
+sleep 1s
+
+echo -e 'Created keypair is passwordless... \n'
+sleep 1s
+
+echo -e 'Showing the created public key below... \n'
+cat ~/.ssh/ansible_connect.pub
+sleep 1s
